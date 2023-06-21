63e0b4c9c21c4a85c2273732b4e51e44394c3f76
Lloyd Allen <lloydallen597@gmail.com>
2023-06-21 16:49:06 -0500

    Implementing the Card Styles and Component

---
+++ src/components/Card/card.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from "react";
+import "./card.styles.css";
+
+class Card extends Component {
+  render() {
+    const { id, name, email } = this.props.monster;
+    return (
+      <div className="card-container" key={id}>
+        <img
+          src={`https://robohash.org/${id}?set=set2size=130x130`}
+          alt={`monster ${name}`}
+        />
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
