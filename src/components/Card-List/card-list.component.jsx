06b364e8851a5796d339751c4bfd13e1036fd3e0
Lloyd Allen <lloydallen597@gmail.com>
2023-06-21 12:21:31 -0500

    Implemented the CardList Component

--- src/components/Card-List/card-list.component.jsx
+++ src/components/Card-List/card-list.component.jsx
@@ -1,15 +1,26 @@
 import { Component } from "react";
+import "./card-list.styles.css";
 
 class CardList extends Component {
   render() {
     const { monsters } = this.props;
 
-    console.log(this.props);
     return (
-      <div>
-        {monsters.map((monster) => (
-          <h1 key={monster.id}>{monster.name}</h1>
-        ))}
+      <div className="card-list">
+        {monsters.map((monster) => {
+          const { name, email, id } = monster;
+
+          return (
+            <div className="card-container" key={id}>
+              <img
+                src={`https://robohash.org/${id}?set=set2size=130x130`}
+                alt={`monster ${name}`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
+        })}
       </div>
     );
   }
